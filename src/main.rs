c897266adb693a94e3522975bceafe4bb6e11093
Tom Craven <tom.cass.craven@gmail.com>
2021-05-18 18:22:12 +0100

    backup

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-extern crate maplit;
+#![allow(dead_code)]
+#![allow(unused_variables)]
+use bit_set::BitSet;
 use std::collections::HashMap;
 use std::fmt::Display;
 
@@ -13,10 +15,49 @@ impl Axial {
     fn new(q: i32, r: i32) -> Self {
         Axial { q, r, z: 0 }
     }
+
+    fn zero() -> Self {
+        Axial::new(0, 0)
+    }
+
+    pub fn surrounding_positions(&self) -> [Axial; 6] {
+        [
+            self.north(),
+            self.north_east(),
+            self.south_east(),
+            self.south(),
+            self.south_west(),
+            self.north_west(),
+        ]
+    }
+
+    pub fn north(&self) -> Axial {
+        Axial::new(self.q, self.r - 1)
+    }
+
+    pub fn north_east(&self) -> Axial {
+        Axial::new(self.q + 1, self.r - 1)
+    }
+
+    pub fn south_east(&self) -> Axial {
+        Axial::new(self.q + 1, self.r)
+    }
+
+    pub fn south(&self) -> Axial {
+        Axial::new(self.q, self.r + 1)
+    }
+
+    pub fn south_west(&self) -> Axial {
+        Axial::new(self.q - 1, self.r + 1)
+    }
+
+    pub fn north_west(&self) -> Axial {
+        Axial::new(self.q - 1, self.r)
+    }
 }
 
 #[derive(Hash, Eq, PartialEq, Debug, Copy, Clone)]
-enum Tile {
+enum TileType {
     Ant,
     Beetle,
     GrassHopper,
@@ -27,6 +68,20 @@ enum Tile {
 enum Colour {
     Black,
     White,
+}
+
+struct Tile {
+    colour: Colour,
+    tile_type: TileType,
+}
+
+impl Tile {
+    fn ant(colour: Colour) -> Self {
+        Tile {
+            tile_type: TileType::Ant,
+            colour,
+        }
+    }
 }
 
 struct Player {}
@@ -56,26 +111,33 @@ impl<T> HexGrid<T> {
 }
 
 struct AvailablePlacements {
-    white: Vec<Axial>,
-    black: Vec<Axial>,
+    // TODO - benchmarks
+    // Available placements are stored as a bitset, where 1 represents a free position, and 0 not free
+    // the point of this is to be fast, not space efficient (though often they come hand in hand)
+    // we want reads and writes to be very fast if we want a speedy MCTS
+    top_left: Axial,
+    bottom_right: Axial,
+    white: BitSet,
+    black: BitSet,
 }
 
 impl Default for AvailablePlacements {
     fn default() -> Self {
+        let white = BitSet::new();
+        let black = BitSet::new();
+
         AvailablePlacements {
-            // TODO: find correct value to use Vec::with_capacity
-            white: Vec::new(),
-            black: Vec::new(),
+            top_left: Axial::zero(),
+            bottom_right: Axial::zero(),
+            white,
+            black,
         }
     }
 }
 
 impl AvailablePlacements {
-    fn placements_for_colour(&self, colour: Colour) -> &Vec<Axial> {
-        match colour {
-            Colour::White => &self.white,
-            Colour::Black => &self.black,
-        }
+    fn placements_for_colour(&self, colour: &Colour) -> &Vec<Axial> {
+        todo!()
     }
 }
 
@@ -94,7 +156,9 @@ impl Default for Game {
 }
 
 impl Game {
-    fn place(&mut self, colour: Colour, position: Axial, tile: Tile) {
+    fn place(&mut self, position: Axial, tile: Tile) {
+        let colour = &tile.colour;
+
         // Debug assertions
         debug_assert!(self
             .get_available_placements(colour)
@@ -108,7 +172,7 @@ impl Game {
 
     fn update_available_placements(&mut self, position: Axial) {}
 
-    fn get_available_placements(&self, colour: Colour) -> impl Iterator<Item = &Axial> {
+    fn get_available_placements(&self, colour: &Colour) -> impl Iterator<Item = &Axial> {
         self.available_placements
             .placements_for_colour(colour)
             .into_iter()
@@ -142,11 +206,12 @@ impl Display for Game {
 fn main() {
     let mut g = Game::default();
 
-    g.place(Colour::White, Axial::new(0, 0), Tile::Ant);
+    g.place(Axial::new(0, 0), Tile::ant(Colour::White));
+    g.place(Axial::new(1, 0), Tile::ant(Colour::Black));
 
     println!(
         "{:?}",
-        g.get_available_placements(Colour::White)
+        g.get_available_placements(&Colour::White)
             .collect::<Vec<&Axial>>()
     );
 
